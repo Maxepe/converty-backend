dce80e887d398c40ba35e8d299ee86969c61df0a
Maxepe <maxipetrei@outlook.com>
2024-11-30 03:44:06 -0300

    Mejoras visuales y correcciones Multiseleccion de plataformas destino

--- frontend/src/app/page/home/home.component.ts
+++ frontend/src/app/page/home/home.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  title = 'Your App Title'
+  activeView: string = 'main'
+  selectedPlatforms: string[] = []
 
-  title = 'NeKonverter';
-  activeView: 'main' | 'url' | 'connect' = 'main';
-
-  setActiveView(view: 'main' | 'url' | 'connect') {
-    this.activeView = view;
+  setActiveView(view: string) {
+    this.activeView = view
   }
 
-  ngOnInit(): void {
+  togglePlatform(platform: string) {
+    const index = this.selectedPlatforms.indexOf(platform)
+    if (index === -1) {
+      this.selectedPlatforms.push(platform)
+    } else {
+      this.selectedPlatforms.splice(index, 1)
+    }
+  }
+
+  isSelected(platform: string): boolean {
+    return this.selectedPlatforms.includes(platform)
   }
 }
